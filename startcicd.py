943f0623b7a0764e4eeb1debae3eddc79dd09303
Peter Plak <peter_plak@dell.com>
2022-10-31 10:29:55 +0100

    updated jobchecker function

--- startcicd.py
+++ startcicd.py
@@ -103,6 +103,8 @@ def jobstatuschecker ( dataobject ):
     status   = ''
     failed   = ''
     finished = ''
+    proceed  = False #This can be used by Jenkins to determine if Chain is continuoud
+    st       = 10 #Delay between check requests
     
     if type(dataobject) == str: dataobject = json.loads(dataobject) #From str to json
  
@@ -110,7 +112,15 @@ def jobstatuschecker ( dataobject ):
     s = settings['awx']
     url = s['prot']+s['serverip']+":"+s['serverport']+urisuffix #create uri for API call to awx to check job status
     myurltuple = ( url, urltuple[1] ) #Create urltuple with url and headers
-    
+   
+    # start Loop, get every 10 seconds jobstatus
+    #
+    # - jobstatus   (can be pending, running, failed)
+    # - jobfailed   (can be false, true)
+    # - jobfinished (can be null or time, i.e 2022-10-24T14:38:50.009531Z)
+
+    print('\n Starting jobchecker. Waiting till AWX template finishes its job...')
+
     while True:
     
         response = request ( myurltuple, "get" ) #Request API call
@@ -122,17 +132,32 @@ def jobstatuschecker ( dataobject ):
                    "jobfinished" : response['finished']
                  }
 
-        status   = result['jobstatus']
+        status   = result['jobstatus'].lower()
         failed   = result['jobfailed']
         finished = result['jobfinished']
     
-        print('Job status : ' + status)
-        if status == 'succesfull' or status == 'failed': break
-        time.sleep(10)
-    
-    print('Job finished')
+        if status == 'succesful':
+            if failed == 'false' or failed == False:
+                print('\n Succesful job finish at ' + finished)
+                proceed = True
+                break
+            else:
+                print('\n Job finished succesful but with failed result.')
+                break
+            cont
+        elif status == 'failed':
+            if finished != None and finished != 'null':
+                print('\n Job finished with "failed" status. Check job logs on AWX. Will not proceed.')
+                break
+            else:
+                print('\n Job finished with "failed" status due to finish errors. Will not proceed.')
 
-    return result #returns the status of the job that was started
+        print('  Job status : ' + status + '. Wait ' + str(st) + ' secs till next check..')
+        time.sleep(st)
+
+    print()
+
+    return proceed #returns the status of the job that was started
 
     
 
@@ -148,7 +173,8 @@ urltuple = return_url ( settings ) #Return required URL, headers if needed & oth
 response = request ( urltuple, "post") #Request API POST request
 #print(response)
 #If AWX project was launched, check its jobstatus till finished
-if 'awx' in urltuple[2]['runtype']: jobstatuschecker ( response ) 
-
+if 'awx' in urltuple[2]['runtype']:
+    checkresult = jobstatuschecker ( response )
+    #print(checkresult)
 
 
